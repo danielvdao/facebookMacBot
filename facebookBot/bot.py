6f79b4935e47467197ab31cb5c68aa49fb9e16f9
Daniel Dao <daniel.vu.dao@gmail.com>
2014-06-23 22:53:22 -0700

    adding heroku variables, lol

--- facebookBot/bot.py
+++ facebookBot/bot.py
@@ -1,8 +1,10 @@
 import facebook 
 from twilio.rest import TwilioRestClient 
 
-# Get the values for everything #
+# Get the values for global objects #
 
-account_sid = 
+# Don't be a dumbass, set heroku variables for your auth tokens and fb account #
+
+account_sid = "ACeda058cd635ee4e2c9c6126740a6fdd8"
 auth_token = 
 client = TwilioRestClient(account_sid, auth_token)
