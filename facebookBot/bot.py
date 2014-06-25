b0709356b929415d08b86902485cec3f25e8cc9f
Daniel Dao <daniel.vu.dao@gmail.com>
2014-06-24 20:41:01 -0700

    testing git ignore

--- facebookBot/bot.py
+++ facebookBot/bot.py
@@ -4,7 +4,14 @@ from twilio.rest import TwilioRestClient
 # Get the values for global objects #
 
 # Don't be a dumbass, set heroku variables for your auth tokens and fb account #
+if 'DYNO' in os.environ:
 
-account_sid = "ACeda058cd635ee4e2c9c6126740a6fdd8"
-auth_token = 
+else:
+	with open('login.properties', 'r') as loginFile:
+		login_info = loginFile.readLines()
+
+	facebook_user = login_info[0].replace('\n','')
+	facebook_pwd = login_info[1].replace('\n','')
+	account_sid = login_info[2].replace('\n','')
+	auth_token = login_info[3].replace('\n','')
 client = TwilioRestClient(account_sid, auth_token)
