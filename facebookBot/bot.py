e95e598048010a2bbbe7ad9f61b262090f604ada
Daniel Dao <daniel.vu.dao@gmail.com>
2014-06-24 20:46:42 -0700

    adding in final heroku variables

--- facebookBot/bot.py
+++ facebookBot/bot.py
@@ -1,11 +1,16 @@
-import facebook 
+import facebook, time, os, sys 
 from twilio.rest import TwilioRestClient 
 
 # Get the values for global objects #
 
-# Don't be a dumbass, set heroku variables for your auth tokens and fb account #
+# If using heroku, use the heroku variables #
 if 'DYNO' in os.environ:
+	facebook_user = os.environ['FACEBOOK_USER']
+	facebook_pwd = os.environ['FACEBOOK_PASSWORD']
+	account_sid = os.environ['TWILIO_SID']
+	auth_token = os.environ['TWILIO_AUTH']
 
+# Else, use the login.properties file #
 else:
 	with open('login.properties', 'r') as loginFile:
 		login_info = loginFile.readLines()
@@ -14,4 +19,5 @@ else:
 	facebook_pwd = login_info[1].replace('\n','')
 	account_sid = login_info[2].replace('\n','')
 	auth_token = login_info[3].replace('\n','')
+
 client = TwilioRestClient(account_sid, auth_token)
