9d33e5e5d90f763e864fb85a2f19f1d51eb7f4e1
Daniel Dao <daniel.vu.dao@gmail.com>
2014-06-26 00:25:45 -0700

    names are now pulling correctly

--- facebookBot/bot.py
+++ facebookBot/bot.py
@@ -21,12 +21,12 @@ else:
 	auth_token = login_info[3].replace('\n','')
 
 # Go to https://developers.facebook.com/tools/explorer and generate an auth token #
-fb_token = "CAACEdEose0cBAKGX2kZCqOOR3CjGHcLK663ijiO448PwDT3HcsfnoeaDzfPJlbZCrrKJhA0KA72ZAYSXhJa6Dq4PMP4JaiWeZBcX5k8ZAFZCHkoh0EmDHUYy5BDVl21uMCCqLFVXvWlQU9qSKmxg8aNPs39KT9ZAsfhP4Ave7LzvZBsUXpZBq6QMWiky8tWZAW36t5ZAzDZCQi3bqAZDZD"
+fb_token = "CAACEdEose0cBAIimRJJZCZC20QHxM8auAsWBM4FCF22p5sDNJJyZBVWku9eKo4ZAirZBabQaZCRtlWgKtcLYi4gSzqM9ATk3JuuOk4dU3NwJgl7x1CUEohXv5bkxPa9grQdTZBhJoAdtlPbflakb3nl5TjlSHw8NlLc0iU2FdIEe4Vr7PZAdbzzhpffAZAvHDujEPNNioZC6Y0XgZDZD"
 
 # Get the group ID also #
 group_id = "381628841954441"
 graph = facebook.GraphAPI(fb_token)
-posts_query = "SELECT post_id, message, permalink, actor_id FROM stream WHERE source_id =" + group_id + " LIMIT 100"
+posts_query = "SELECT post_id, message, permalink, actor_id FROM stream WHERE source_id =" + group_id + " LIMIT 50"
 
 client = TwilioRestClient(account_sid, auth_token)
 
@@ -35,24 +35,24 @@ def posts():
 	# Getting all the comments...  #
 	post_wall = graph.fql(query=posts_query)
 	names = []
-	links = []
 	item_count = 0
 	
 	# Get the item counts and the links for each post #
 	for post in post_wall:
 		msg = str(post['message'])
-		test = str(post['permalink'])
 		name_query = "SELECT first_name, last_name FROM user WHERE uid =" + str(post['actor_id'])
 		name_query = graph.fql(query=name_query)
 		
-		if 'macbook' in msg.lower() and 'buying' not in msg.lower():
+		if 'macbook' in msg.lower():
+			name_list = name_query[0]
+			temp_name = str(name_list['first_name']) + ' ' + str(name_list['last_name'])
 			item_count += 1
 
-	print names
-	# send_txt(item_count)
+	# send_txt(item_count, names)
 
 # Function to send the text message using twilio #
-def send_txt(item_count, names, links):
+def send_txt(item_count, names):
+
 	item_count = str(item_count)
 	message = client.messages.create(to="+18322739257", from_="+18326102549", body="in the last 100 sales there were " + item_count + " Macbook posts.")
 
